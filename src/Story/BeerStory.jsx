150dd484d5bb7fcf3d1c76f04abb20f739ce6fcc
Hongaproject <tkwlscjq99@gmail.com>
2025-06-08 14:08:51 +0900

    story/beer, makgeolli 부분 코드 수정

--- src/Story/BeerStory.jsx
+++ src/Story/BeerStory.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -12,204 +11,114 @@ const Container = styled.div`
     }
 `;
 
-const Introduce = styled.div`
+const TitleMain = styled.div`
     width: 100%;
-    height: 120px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 110px;
-`;
-const IntroduceTitle = styled.h1`
-    width: 230px;
-    height: 80px;
-    border: 1px solid #000;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: ${(props) => (props.active ? "#87CEEB" : "#FFFFFF")};
-    box-shadow: 5px 3px 0px rgba(0, 0, 0, 0.25);
-    border-radius: 50px;
-    margin-right: 70px;
-    font-size: 32px;
-    cursor: pointer;
-    transition: background 0.3s ease;
-    color: #000;
-
-    @media (max-width: 768px) {
-        width: 110px;
-        height: 40px;
-        margin-right: 10px;
-        font-size: 1.1rem;
-    }
-`;
-
-const Summary = styled.div`
-    width: calc(100% - 440px);
-    height: 100%;
+    max-width: 1200px;
     margin: 0 auto;
+    margin-top: 250px;
     margin-bottom: 150px;
+    height: auto;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    box-sizing: border-box;
-
-    @media (max-width: 768px) {
-        width: 100%;
-        padding: 0 20px;
-    }
 `;
-const HistoryText = styled.div`
-    width: 100%;
-    height: 100%;
-    margin-top: 90px;
-
-    @media (max-width: 768px) {
-        text-align: center;
-    }
-`;
-const HistoryTitle = styled.h1`
+const Title = styled.h1`
     font-size: 48px;
-    margin-bottom: 40px;
+    text-align: center;
 `;
-const HistoryContent = styled.span`
+const TitleNav = styled.nav`
+    width: 800px;
+    height: auto;
+    border-bottom: 2px solid #000;
+    margin-top: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 80px;
+`;
+const TitleNavL = styled.div`
+    font-size: 22px;
+    padding-bottom: 20px;
+    color: ${({ active }) => (active ? "#C98B20" : "#000")};
+`;
+
+const TitleSub = styled.h2`
+    font-size: 40px;
+    text-align: center;
+    margin-top: 150px;
+    color: #c98b20;
+`;
+
+const TitleDes = styled.h1`
     font-size: 20px;
-    color: #909090;
-    line-height: 1.4;
-
-    @media (max-width: 768px) {
-        font-size: 18px;
-        text-align: center;
-        display: block;
-    }
-`;
-const HistoryText2 = styled.div`
-    width: 100%;
-    height: 100%;
-    margin-top: 40px;
-
-    @media (max-width: 768px) {
-        text-align: center;
-    }
+    text-align: center;
+    margin-top: 70px;
+    line-height: 2;
 `;
 
-const BeerGuide = styled.button`
-    width: 310px;
-    height: 58px;
-    background-color: #000;
-    color: #fff;
+const SojuGuide = styled.button`
+    width: 400px;
+    height: 85px;
+    background-color: #eacaa2;
+    color: #000;
     font-size: 24px;
     cursor: pointer;
+    margin: 0 auto;
     margin-top: 70px;
+    border-radius: 204px;
+    border: none;
 `;
 
 export default function BeerStory() {
-    const location = useLocation();
-    const [activeTitle, setActiveTitle] = useState("");
-
-    useEffect(() => {
-        switch (location.pathname) {
-            case "/story/beer":
-                setActiveTitle("맥주의 역사");
-                break;
-            default:
-                setActiveTitle("");
-        }
-    }, [location.pathname]);
-
-    const handleClick = (title) => {
-        setActiveTitle(title);
-    };
-
     return (
         <Container>
-            <Introduce>
-                <Link
-                    to="/story"
-                    style={{ textDecoration: "none", color: "#000" }}
-                    aria-label="개요 페이지로 이동"
-                >
-                    <IntroduceTitle
-                        active={activeTitle === "개요"}
-                        onClick={() => handleClick("개요")}
+            <TitleMain>
+                <Title>주류의 역사</Title>
+                <TitleNav>
+                    <Link to="/story/soju" style={{ textDecoration: "none" }}>
+                        <TitleNavL>소주</TitleNavL>
+                    </Link>
+                    <Link to="/story/beer" style={{ textDecoration: "none" }}>
+                        <TitleNavL active={true}>맥주</TitleNavL>
+                    </Link>
+                    <Link
+                        to="/story/makgeolli"
+                        style={{ textDecoration: "none" }}
                     >
-                        개요
-                    </IntroduceTitle>
+                        <TitleNavL>막걸리</TitleNavL>
+                    </Link>
+                </TitleNav>
+                <TitleSub>맥주</TitleSub>
+                <TitleDes>
+                    맥주는 B.C 4000년경 중동 지방의 티그리스, 유프라테스 강
+                    유역에서 수메르 민족이 최초로 제조하였던 것으로 알려져
+                    있습니다.
+                    <br />
+                    현재 고대 이집트를 탐사하고 있는 고고학자들이 새로운 정보를
+                    알아내고 있습니다. 고대 이집트 당시 대형 양조장이 있었다고
+                    합니다. <br />
+                    현재 우리가 아는 맥주 제조방법은 독일에서 시작되었으며
+                    독일로 여행을 가신다면 몇백년 된 양조장들을 구경할 수
+                    있습니다.
+                </TitleDes>
+                <TitleDes>
+                    19세기 맥주에 큰 변화가 왔었습니다. 맥주의 대량 생산을
+                    가능하게 해줬으며, 카를 폰 린데 독일인이 냉동기를 제작하여
+                    계절에 상관없이 양조를 가능하게 했다. <br />
+                    루이 파스퇴르 프랑스인이 술이 효모의 작용에 의해 생성된다는
+                    사실과 열처리 살균법을 개발하여 오랫동안 보관이 가능하게
+                    되었습니다. <br />
+                    이후 에밀 한센 덴마크인이 파스퇴르의 이론을 응용해 효모의
+                    순수배양법을 개발하면서 맥주의 품질을 높였습니다. <br />
+                    80년대 이후 맥주 고유의 신선도를 유지하면서 장기 유통을 할
+                    수 있는 첨단 비열처리 공법이 개발되면서 소비자들은 더욱
+                    신선한 맥주를 즐길 수 있게 되었습니다.
+                </TitleDes>
+                <Link to="/guide/soju" aria-label="맥주 가이드 페이지로 이동">
+                    <SojuGuide>맥주 가이드 확인하기</SojuGuide>
                 </Link>
-                <Link
-                    to="/story/soju"
-                    style={{ textDecoration: "none", color: "#000" }}
-                    aria-label="소주의 역사 페이지로 이동"
-                >
-                    <IntroduceTitle
-                        active={activeTitle === "소주의 역사"}
-                        onClick={() => handleClick("소주의 역사")}
-                    >
-                        소주의 역사
-                    </IntroduceTitle>
-                </Link>
-                <Link
-                    to="/story/beer"
-                    style={{ textDecoration: "none", color: "#000" }}
-                    aria-label="맥주의 역사 페이지로 이동"
-                >
-                    <IntroduceTitle
-                        active={activeTitle === "맥주의 역사"}
-                        onClick={() => handleClick("맥주의 역사")}
-                    >
-                        맥주의 역사
-                    </IntroduceTitle>
-                </Link>
-                <Link
-                    to="/story/makgeolli"
-                    style={{ textDecoration: "none", color: "#000" }}
-                    aria-label="막걸리의 역사 페이지로 이동"
-                >
-                    <IntroduceTitle
-                        active={activeTitle === "막걸리의 역사"}
-                        onClick={() => handleClick("막걸리의 역사")}
-                    >
-                        막걸리의 역사
-                    </IntroduceTitle>
-                </Link>
-            </Introduce>
-            <Summary>
-                <HistoryText aria-labelledby="맥주의 역사">
-                    <HistoryTitle id="맥주의 역사">맥주의 역사</HistoryTitle>
-                    <HistoryContent>
-                        맥주는 B.C 4000년경 중동 지방의 티그리스, 유프라테스 강
-                        유역에서 수메르 민족이 최초로 제조하였던 것으로 알려져
-                        있습니다.
-                        <br />
-                        현재 고대 이집트를 탐사하고 있는 고고학자들이 새로운
-                        정보를 알아내고 있습니다. 고대 이집트 당시 대형 양조장이
-                        있었다고 합니다. <br />
-                        현재 우리가 아는 맥주 제조방법은 독일에서 시작되었으며
-                        독일로 여행을 가신다면 몇백년 된 양조장들을 구경할 수
-                        있습니다.
-                    </HistoryContent>
-                </HistoryText>
-                <HistoryText2>
-                    <HistoryContent>
-                        19세기 맥주에 큰 변화가 왔었습니다. 맥주의 대량 생산을
-                        가능하게 해줬으며, 카를 폰 린데 독일인이 냉동기를
-                        제작하여 계절에 상관없이 양조를 가능하게 했다. <br />
-                        루이 파스퇴르 프랑스인이 술이 효모의 작용에 의해
-                        생성된다는 사실과 열처리 살균법을 개발하여 오랫동안
-                        보관이 가능하게 되었습니다. <br />
-                        이후 에밀 한센 덴마크인이 파스퇴르의 이론을 응용해
-                        효모의 순수배양법을 개발하면서 맥주의 품질을 높였습니다.{" "}
-                        <br />
-                        80년대 이후 맥주 고유의 신선도를 유지하면서 장기 유통을
-                        할 수 있는 첨단 비열처리 공법이 개발되면서 소비자들은
-                        더욱 신선한 맥주를 즐길 수 있게 되었습니다.
-                    </HistoryContent>
-                </HistoryText2>
-                <Link to="/guide/beer" aria-label="맥주 가이드 페이지로 이동">
-                    <BeerGuide>맥주 가이드 확인하기</BeerGuide>
-                </Link>
-            </Summary>
+            </TitleMain>
         </Container>
     );
 }
--- src/Story/MakgeolliStory.jsx
+++ src/Story/MakgeolliStory.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -12,206 +11,111 @@ const Container = styled.div`
     }
 `;
 
-const Introduce = styled.div`
+const TitleMain = styled.div`
     width: 100%;
-    height: 120px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 110px;
-`;
-const IntroduceTitle = styled.h1`
-    width: 230px;
-    height: 80px;
-    border: 1px solid #000;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: ${(props) => (props.active ? "#87CEEB" : "#FFFFFF")};
-    box-shadow: 5px 3px 0px rgba(0, 0, 0, 0.25);
-    border-radius: 50px;
-    margin-right: 70px;
-    font-size: 32px;
-    cursor: pointer;
-    transition: background 0.3s ease;
-    color: #000;
-
-    @media (max-width: 768px) {
-        width: 110px;
-        height: 40px;
-        margin-right: 10px;
-        font-size: 1.1rem;
-    }
-`;
-
-const Summary = styled.div`
-    width: calc(100% - 440px);
-    height: 100%;
+    max-width: 1200px;
     margin: 0 auto;
+    margin-top: 250px;
     margin-bottom: 150px;
+    height: auto;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
-    box-sizing: border-box;
-
-    @media (max-width: 768px) {
-        width: 100%;
-        padding: 0 20px;
-    }
 `;
-const HistoryText = styled.div`
-    width: 100%;
-    height: 100%;
-    margin-top: 90px;
-
-    @media (max-width: 768px) {
-        text-align: center;
-    }
-`;
-const HistoryTitle = styled.h1`
+const Title = styled.h1`
     font-size: 48px;
-    margin-bottom: 40px;
+    text-align: center;
 `;
-const HistoryContent = styled.span`
+const TitleNav = styled.nav`
+    width: 800px;
+    height: auto;
+    border-bottom: 2px solid #000;
+    margin-top: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 80px;
+`;
+const TitleNavL = styled.div`
+    font-size: 22px;
+    padding-bottom: 20px;
+    color: ${({ active }) => (active ? "#0066D3" : "#000")};
+`;
+
+const TitleSub = styled.h2`
+    font-size: 40px;
+    text-align: center;
+    margin-top: 150px;
+    color: #0066d3;
+`;
+
+const TitleDes = styled.h1`
     font-size: 20px;
-    color: #909090;
-    line-height: 1.4;
-
-    @media (max-width: 768px) {
-        font-size: 18px;
-        text-align: center;
-        display: block;
-    }
+    text-align: center;
+    margin-top: 70px;
+    line-height: 2;
 `;
-const HistoryText2 = styled.div`
-    width: 100%;
-    height: 100%;
-    margin-top: 40px;
 
-    @media (max-width: 768px) {
-        text-align: center;
-    }
-`;
-const BeerGuide = styled.button`
-    width: 310px;
-    height: 58px;
-    background-color: #000;
-    color: #fff;
+const SojuGuide = styled.button`
+    width: 400px;
+    height: 85px;
+    background-color: #c0deff;
+    color: #000;
     font-size: 24px;
     cursor: pointer;
+    margin: 0 auto;
     margin-top: 70px;
+    border-radius: 204px;
+    border: none;
 `;
 
 export default function MakgeolliStory() {
-    const location = useLocation();
-    const [activeTitle, setActiveTitle] = useState("");
-
-    useEffect(() => {
-        switch (location.pathname) {
-            case "/story/makgeolli":
-                setActiveTitle("막걸리의 역사");
-                break;
-            default:
-                setActiveTitle("");
-        }
-    }, [location.pathname]);
-
-    const handleClick = (title) => {
-        setActiveTitle(title);
-    };
-
     return (
         <Container>
-            <Introduce>
-                <Link
-                    to="/story"
-                    style={{ textDecoration: "none", color: "#000" }}
-                    aria-label="개요 페이지로 이동"
-                >
-                    <IntroduceTitle
-                        active={activeTitle === "개요"}
-                        onClick={() => handleClick("개요")}
+            <TitleMain>
+                <Title>주류의 역사</Title>
+                <TitleNav>
+                    <Link to="/story/soju" style={{ textDecoration: "none" }}>
+                        <TitleNavL>소주</TitleNavL>
+                    </Link>
+                    <Link to="/story/beer" style={{ textDecoration: "none" }}>
+                        <TitleNavL>맥주</TitleNavL>
+                    </Link>
+                    <Link
+                        to="/story/makgeolli"
+                        style={{ textDecoration: "none" }}
                     >
-                        개요
-                    </IntroduceTitle>
+                        <TitleNavL active={true}>막걸리</TitleNavL>
+                    </Link>
+                </TitleNav>
+                <TitleSub>막걸리</TitleSub>
+                <TitleDes>
+                    막걸리는 대한민국에서 가장 오래된 술 입니다. 삼국시대부터
+                    양조가 된거로 추정을하고 있으며 고려 시대 서적을 보면 탁주
+                    이야기가 적혀져 있습니다. <br />
+                    소주가 나타나기전에 대한민국 주류 시장은 탁주가 높은
+                    점유율을 가지고 있었습니다. 하지만 60년대 70년대부터 주세법
+                    개정으로 인하여 주류에 쌀이 금지가 되었습니다. <br />
+                    이로 인해 막걸리의 점유율은 낮아졌으며 희석주 소주와 맥주의
+                    등장으로 인기가 식어갔습니다. <br />
+                </TitleDes>
+                <TitleDes>
+                    현재 100% 쌀로 만들고 있으며 21세기에 들어서 다시 인기를
+                    되찾고 있습니다. 막걸리는 도수가 희석주보다 낮고 또한 영양
+                    성분이 좋습니다. <br />
+                    최근 수십년간 문화 전통에 관심이 많아지게 되었고 또한 젊은
+                    세대에서 선호도가 증가가 되고 있습니다. <br />
+                    2021년 대한민국 문화재청에서는 '막걸리 빚기'를
+                    국가무형문화재 신규 종목으로 지정했는데, 국민의 제안을
+                    수용하여 지정된 첫 번째 사례입니다. <br />
+                    현재도 UNESCO 세계무형문화유산으로 등재하기 위한 노력을 하고
+                    있습니다.
+                </TitleDes>
+                <Link to="/guide/soju" aria-label="막걸리 가이드 페이지로 이동">
+                    <SojuGuide>막걸리 가이드 확인하기</SojuGuide>
                 </Link>
-                <Link
-                    to="/story/soju"
-                    style={{ textDecoration: "none", color: "#000" }}
-                    aria-label="소주의 역사 페이지로 이동"
-                >
-                    <IntroduceTitle
-                        active={activeTitle === "소주의 역사"}
-                        onClick={() => handleClick("소주의 역사")}
-                    >
-                        소주의 역사
-                    </IntroduceTitle>
-                </Link>
-                <Link
-                    to="/story/beer"
-                    style={{ textDecoration: "none", color: "#000" }}
-                    aria-label="맥주의 역사 페이지로 이동"
-                >
-                    <IntroduceTitle
-                        active={activeTitle === "맥주의 역사"}
-                        onClick={() => handleClick("맥주의 역사")}
-                    >
-                        맥주의 역사
-                    </IntroduceTitle>
-                </Link>
-                <Link
-                    to="/story/makgeolli"
-                    style={{ textDecoration: "none", color: "#000" }}
-                    aria-label="막걸리의 역사 페이지로 이동"
-                >
-                    <IntroduceTitle
-                        active={activeTitle === "막걸리의 역사"}
-                        onClick={() => handleClick("막걸리의 역사")}
-                    >
-                        막걸리의 역사
-                    </IntroduceTitle>
-                </Link>
-            </Introduce>
-            <Summary>
-                <HistoryText aria-labelledby="막걸리의 역사">
-                    <HistoryTitle id="막걸리의 역사">
-                        막걸리의 역사
-                    </HistoryTitle>
-                    <HistoryContent>
-                        막걸리는 대한민국에서 가장 오래된 술 입니다.
-                        삼국시대부터 양조가 된거로 추정을하고 있으며 고려 시대
-                        서적을 보면 탁주 이야기가 적혀져 있습니다. <br />
-                        소주가 나타나기전에 대한민국 주류 시장은 탁주가 높은
-                        점유율을 가지고 있었습니다. 하지만 60년대 70년대부터
-                        주세법 개정으로 인하여 주류에 쌀이 금지가 되었습니다.{" "}
-                        <br />
-                        이로 인해 막걸리의 점유율은 낮아졌으며 희석주 소주와
-                        맥주의 등장으로 인기가 식어갔습니다. <br />
-                    </HistoryContent>
-                </HistoryText>
-                <HistoryText2>
-                    <HistoryContent>
-                        현재 100% 쌀로 만들고 있으며 21세기에 들어서 다시 인기를
-                        되찾고 있습니다. 막걸리는 도수가 희석주보다 낮고 또한
-                        영양 성분이 좋습니다. <br />
-                        최근 수십년간 문화 전통에 관심이 많아지게 되었고 또한
-                        젊은 세대에서 선호도가 증가가 되고 있습니다. <br />
-                        2021년 대한민국 문화재청에서는 '막걸리 빚기'를
-                        국가무형문화재 신규 종목으로 지정했는데, 국민의 제안을
-                        수용하여 지정된 첫 번째 사례입니다. <br />
-                        현재도 UNESCO 세계무형문화유산으로 등재하기 위한 노력을
-                        하고 있습니다.
-                    </HistoryContent>
-                </HistoryText2>
-                <Link
-                    to="/guid/makgeolli"
-                    aria-label="막걸리 가이드 페이지로 이동"
-                >
-                    <BeerGuide>막걸리 가이드 확인하기</BeerGuide>
-                </Link>
-            </Summary>
+            </TitleMain>
         </Container>
     );
 }
